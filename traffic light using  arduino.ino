59e7eb66352e8c32202fbcc064eb53b67471f11f
Shashank Katti <90169675+Shashank26112@users.noreply.github.com>
2024-01-21 12:55:08 +0530

    Update traffic light using  arduino.ino

--- traffic light using  arduino.ino
+++ traffic light using  arduino.ino
@@ -1,20 +1,31 @@
 #include <toneAC.h>
+#include <Talkie.h>
+
 int red = 9;
 int yellow = 8;
 int green = 7;
 int buzzerPin = 6; // Replace 6 with the actual pin number you connect the buzzer to
+
+Talkie voice;
 
 void setup() {
   pinMode(red, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
+  
+  voice.begin();
+
+  // Load the voice data for "Stop" and "Go"
+  voice.say("Stop");
+  voice.say("Go");
 }
 
 void loop() {
   // Red light - Stop
   digitalWrite(red, HIGH);
   playTone(buzzerPin, 1000, 1500); // Voice: "Stop"
+  voice.say("Stop");  // Use Talkie to say "Stop"
   digitalWrite(red, LOW);
 
   // Yellow light
@@ -28,6 +39,7 @@ void loop() {
   // Green light - Go
   digitalWrite(green, HIGH);
   playTone(buzzerPin, 1500, 1500); // Voice: "Go"
+  voice.say("Go");  // Use Talkie to say "Go"
   digitalWrite(green, LOW);
 
   // Yellow light
