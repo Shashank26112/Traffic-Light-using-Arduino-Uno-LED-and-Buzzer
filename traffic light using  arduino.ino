8df11466ba58ed0edf37def8e1c1381001cbc68a
Shashank KATTI <90169675+Shashank26112@users.noreply.github.com>
2024-02-29 22:28:32 +0530

    Dynamic traffic.ino

--- traffic light using  arduino.ino
+++ traffic light using  arduino.ino
@@ -1,48 +1,279 @@
 #include <toneAC.h>
 #include <Talkie.h>
+int g1=7;//green
+int r1=6;//red
 
-#define ledr 7
-#define ledg 9
-#define ledY 8
-#define voice1 10
-#define voice2 5
+int r2=8;//red
+int g2=9;//green
 
-#define buz 6
+int g3=11;//green
+int r3=10;//red
 
-void setup() {
-  // put your setup code here, to run once:
+int r4=12;//red
+int g4=13;//green
+
+int ir1=2;//sensor 1
+int ir2=3;//sensor 2
+int ir3=4;//sensor 3
+int ir4=5;//sensor 4
+
+void setup() 
+
+{
+  
 Serial.begin(9600);
-pinMode(ledr,OUTPUT);
-pinMode(ledg,OUTPUT);
-pinMode(ledY,OUTPUT);
-pinMode(buz,OUTPUT);
-pinMode(voice1,OUTPUT);
-pinMode(voice2,OUTPUT);
+ 
+pinMode(g1,OUTPUT);
+pinMode(r1,OUTPUT);
+pinMode(g2,OUTPUT);
+pinMode(r2,OUTPUT);
+pinMode(g3,OUTPUT);
+pinMode(r3,OUTPUT);
+pinMode(g4,OUTPUT);
+pinMode(r4,OUTPUT);
 
+pinMode(ir1,INPUT);
+pinMode(ir2,INPUT);
+pinMode(ir3,INPUT);
+pinMode(ir4,INPUT);
+
+   
 }
 
 void loop() 
+
 {
-digitalWrite(voice2, 1);
-digitalWrite(voice1, 1);
-delay(1000);
-digitalWrite(ledr, 1);
-digitalWrite(buz,HIGH);
-digitalWrite(voice1, 0);
-delay(2000);
-digitalWrite(voice1, 1);
-digitalWrite(buz,LOW);
-digitalWrite(ledr, 0);
-delay(2000);
-digitalWrite(ledY, 1);
-delay(2000);
-digitalWrite(ledY, 0);
-delay(2000);
- 
-digitalWrite(ledg, 1);
-digitalWrite(voice2, 0);
-delay(1000);
-digitalWrite(voice2, 1);
-digitalWrite(ledg, 0);
-delay(3000);
+  int irsensor1=digitalRead(ir1);
+    int irsensor2=digitalRead(ir2);
+      int irsensor3=digitalRead(ir3);
+        int irsensor4=digitalRead(ir4);
+
+  if (irsensor1==0)
+  {
+    digitalWrite(g1,1);//green
+    digitalWrite(g2,1);//green
+
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,1);//red
+  }
+    if (irsensor2==0)
+  {
+    digitalWrite(g3,1);//green
+    digitalWrite(g2,1);//green
+
+    digitalWrite(r1,1);//red
+    digitalWrite(r4,1);//red
+  }
+
+   if(irsensor3==0)
+  {
+    digitalWrite(g4,1);//green
+    digitalWrite(g3,1);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    
+  }
+
+    if(irsensor4==0)
+  {
+    digitalWrite(g1,1);//green
+    digitalWrite(g4,1);//green
+    
+    digitalWrite(r3,1);//red
+    digitalWrite(r2,1);//red
+    
+  }
+  else 
+  {
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,0);//red
+    digitalWrite(r2,0);//red
+    digitalWrite(r3,0);//red
+    digitalWrite(r4,0);//red
+  }
+  
+   if((irsensor1==0)&&(irsensor2==0)) 
+  {
+    signal1();
+  }
+  if((irsensor2==0)&&(irsensor3==0)) 
+  {
+    signal2();
+  }
+  if((irsensor3==0)&&(irsensor4==0)) 
+  {
+    signal3();
+  }
+   if((irsensor1==0)&&(irsensor4==0)) 
+  {
+    signal4();
+  }
+  if((irsensor1==0)&&(irsensor3==0)) 
+  {
+    signal5();
+  }
+  if((irsensor2==0)&&(irsensor4==0)) 
+  {
+    signal6();
+  }
+}
+void signal1()
+{
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,1);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,0);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,1);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,0);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+}
+
+void signal2()
+{
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,1);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,0);//red
+    delay(5000);
+    
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,1);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,0);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+}
+
+void signal3()
+{
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,1);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,0);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,1);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,0);//red
+    delay(5000);
+    
+}
+
+void signal4()
+{
+    digitalWrite(g1,1);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,0);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,1);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,0);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+}
+
+void signal5()
+{
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,1);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,0);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,1);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,0);//red
+    delay(5000);
+    
+}
+
+void signal6()
+{
+    digitalWrite(g1,1);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,0);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,0);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,1);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
+    digitalWrite(g1,0);//green
+    digitalWrite(g2,0);//green
+    digitalWrite(g3,1);//green
+    digitalWrite(g4,0);//green
+    
+    digitalWrite(r1,1);//red
+    digitalWrite(r2,1);//red
+    digitalWrite(r3,0);//red
+    digitalWrite(r4,1);//red
+    delay(5000);
+    
 }
